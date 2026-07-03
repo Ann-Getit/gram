17c040186b0148f40ede32515bb0c70eb9935d03
Ann-Getit <Ann-Getit+182119360+Ann-Getit@users.noreply.github.com>
2026-07-03 16:15:36 +0200

    the profilepic-and-followers layout is done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,9 @@
 import "./Profile.css";
 /*import { Link } from "react-router-dom"*/
 
-import { TbCameraPlus } from "react-icons/tb";
+import { IoCamera } from "react-icons/io5";
 
+import { HiMiniChatBubbleLeft } from "react-icons/hi2";
 
 const Profile = () => {
 
@@ -14,16 +15,57 @@ const Profile = () => {
 
 
        <div className="profilepic-and-followers">
-
+        
         <div className="profile_picture">
-            <button><TbCameraPlus /></button>
+
+            <button className="cloudtext-button" >
+            <span>Type iets..</span>
+            </button>
+
+
+            <button className="profile-camera-button">
+                <IoCamera />
+            </button>
+        </div>
+
+
+        <div className="followers">
+            <button className="followers-profile">
+                <span className="totaal">0</span>
+                <span className="tekst-profile">berichten</span>
+            </button>
+
+             <button className="followers-profile">
+                <span className="totaal">30</span>
+                <span className="tekst-profile">volgers</span>
+            </button>
+
+             <button className="followers-profile">
+                <span className="totaal">29</span>
+                <span className="tekst-profile" >volgend</span>
+            </button>
+
+        </div>
+
+       </div>
+
+
+
+
+           {/*} <div className="profilepic-and-followers">
+        
+        <div className="profile_picture">
+
+            <input type="text" placeholder="Typ iets..." className="bubble" />
+            <HiMiniChatBubbleLeft  className="bubbles-tekst-profile"/>
+            <button><IoCamera /></button>
         </div>
 
         <div className="followers">
 
         </div>
 
-       </div>
+       </div>*/}
         </>
     )
 }
