45a9231d22a5ab4a5756c555a53c79577f1199a8
Ann-Getit <182119360+Ann-Getit@users.noreply.github.com>
2026-07-18 01:46:44 +0200

    input

--- src/pages/Chatsection.jsx
+++ src/pages/Chatsection.jsx
@@ -3,24 +3,40 @@ import { useState } from "react"
 
 import { RiSearchAiLine } from "react-icons/ri";
 import { CiLocationArrow1 } from "react-icons/ci";
+import { IoIosArrowBack } from "react-icons/io";
+
+
 const Chatsection = () => {
 
 const [showButton, setShowButton] = useState(false);
 
-    
+const [showArrowChat, setShowArrowChat] = useState(false);
+
 
 
     return (
         <>
   <div className="chatsearch">
-       <RiSearchAiLine className="searchicon"/>
+    {showArrowChat && (
+    <IoIosArrowBack className="showklikarrow"/>
+    )}
+    <RiSearchAiLine className="searchicon"/>
 
-    <input type="text"  placeholder= "Zoeken of vragen aan Meta AI" 
-    onFocus={() => setShowButton(true)}
-  onBlur={() => setShowButton(false)}
-    className="inputesearch"/> 
+    <input type="text"  
+    placeholder= "Zoeken of vragen aan Meta AI" 
+    className="inputesearch"
+    onFocus={() => {
+        setShowButton(true);
+         setShowArrowChat(true);
+    }}
+     onBlur={() => {
+    setShowButton(false);
+    setShowArrowChat(false);
+     }}
+    />
 
      {showButton && (
+
     <div>
     |<button 
     
