91798797be08ee8253e61c1943817a9dda5a606b
Ann-Getit <182119360+Ann-Getit@users.noreply.github.com>
2026-07-18 16:19:11 +0200

    added de verzoeken link

--- src/pages/Chatsection.jsx
+++ src/pages/Chatsection.jsx
@@ -1,10 +1,11 @@
 import "./Chatsection.css";
 import { useState } from "react"
-
+import { Link } from "react-router-dom"
 import { RiSearchAiLine } from "react-icons/ri";
 import { CiLocationArrow1 } from "react-icons/ci";
 import { IoIosArrowBack } from "react-icons/io";
-
+import { IoPersonCircleSharp } from "react-icons/io5";
+import { GoBellSlash } from "react-icons/go";
 
 const Chatsection = () => {
 
@@ -44,7 +45,33 @@ const [showArrowChat, setShowArrowChat] = useState(false);
    <CiLocationArrow1 className="arrowforinput" />
    </button>
    </div>)}
+    </div>
 
+
+    <div className="chatlist">
+        <div className="verhaalboxjeschat">
+            <button className="cloudtext-chat" >
+            <span>Type iets..</span> {/*meerdere tekst toevoegen*/}
+            </button>
+                    <button className="verhaalvoegenchat">
+                        <IoPersonCircleSharp className="person-icon-verhaal"/>
+                    </button>
+                    <span className="names-homechat">je notitie</span>
+                    </div>
+    </div>
+
+    <div className="verzoekchat">
+        <div>
+            <p className="chatbericht-chat">Chatberichten <GoBellSlash className="nobell"/></p>
+        </div>
+
+        <div>
+            <Link to="#" >Verzoeken</Link>
+        </div>
+    </div>
+
+    <div className="lists-chat">
+        
     </div>
         </>
     )
